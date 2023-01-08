b470820f0d37724449b458e8558a7df1e648dc1c
Shashanka-G <shashanka.cs20@bmsce.ac.in>
2023-01-08 20:41:07 +0530

    final commit

--- 02-01-2023/findword.sh
+++ 02-01-2023/findword.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-#for pattern in $@
-#do
-#true
-#done
+for pattern in $@
+do
+true
+done
 
-pattern=${*:-1:1}
+total_files=$(($# - 1))
 
-echo file pattern is $pattern
+echo "total files $total_files"
+echo "pattern is $pattern"
+
+i=1
+
+while [ $i -le $total_files ]
+do
+echo "searching $pattern in $1"
+grep $pattern $1
+
+if [ $? -eq 0 ];then
+  echo "$pattern found in $1"
+  exit 0
+fi
+
+shift
+i=$(($i + 1))
+done
